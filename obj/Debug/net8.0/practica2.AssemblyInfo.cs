b6610200498a5499bf959d8bcc8cd4404ddb0cf0
eduardotr2810 <eduardotr2810mail.com>
2025-05-02 01:20:43 -0500

    adopcionlista

--- obj/Debug/net8.0/practica2.AssemblyInfo.cs
+++ obj/Debug/net8.0/practica2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practica2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb4d517b2fc21468493c9e32b756b879d8eaa80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+289d4d0b7bce1384b9593ed3c05fe8ba3b41e3cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("practica2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practica2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
